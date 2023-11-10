dd663bddccceaee7301f7674c7a61b2352e86b93
Elarif21 <150395161+Elarif21@users.noreply.github.com>
2023-11-10 09:51:53 +0200

    Add files via upload

---
+++ pg_env.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+REM The script sets environment variables helpful for PostgreSQL
+
+@SET PATH="G:\odoo\PostgreSQL\bin";%PATH%
+@SET PGDATA=G:\odoo\PostgreSQL\data
+@SET PGDATABASE=postgres
+@SET PGUSER=amr
+@SET PGPORT=5432
+@SET PGLOCALEDIR=G:\odoo\PostgreSQL\share\locale
+
+                            
